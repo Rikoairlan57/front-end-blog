088e2d57275a009ef7afdc27dd69ce28bc15a6c5
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2022-11-18 08:18:31 +0700

    adding compiler

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/display-name */
 /* eslint-disable import/no-anonymous-default-export */
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/index.scss";
+import "styles/index.scss";
 
 export default ({ Component, pageProps }) => <Component {...pageProps} />;
