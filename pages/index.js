4db6e00e88cba1b86ed994380c41294661ea1813
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2022-11-20 07:00:26 +0700

    get all blog in json by sanity

--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,33 @@ import AuthorIntro from "components/AuthorIntro";
 import CardItem from "components/CardItem";
 import CardListItem from "components/CardListItem";
 
-export default function Home() {
+import { getAllBlogs } from "lib/api";
+
+export default function Home({ blogs }) {
   return (
     <PageLayout>
       <AuthorIntro />
       <hr />
       <Row className="mb-5">
-        <Col md="10">
+        {/* <Col md="10">
           <CardListItem />
-        </Col>
-        <Col md="4">
-          <CardItem />
-        </Col>
+        </Col> */}
+        {blogs.map((blog) => (
+          <Col key={blog.slug} md="4">
+            <CardItem title={blog.title} subtitle={blog.subtitle} />
+          </Col>
+        ))}
       </Row>
     </PageLayout>
   );
+}
+
+export async function getStaticProps() {
+  console.log("Calling getStaticProps");
+  const blogs = await getAllBlogs();
+  return {
+    props: {
+      blogs,
+    },
+  };
 }
