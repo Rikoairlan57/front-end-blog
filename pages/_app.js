61f87cc02325c48f27a7063512bcd608f7c59b2a
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2022-11-29 20:08:45 +0700

    add font awesome

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 /* eslint-disable react/display-name */
 /* eslint-disable import/no-anonymous-default-export */
+import { library, config } from "@fortawesome/fontawesome-svg-core";
+import { faBorderAll, faList } from "@fortawesome/free-solid-svg-icons";
+
+config.autoAddCss = false;
+library.add(faList, faBorderAll);
+
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "highlight.js/styles/darcula.css";
 import "styles/index.scss";
