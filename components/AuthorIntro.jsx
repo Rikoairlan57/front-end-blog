777c80d95686893cc791ef5ac12ef86efde05dae
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2022-12-03 20:42:30 +0700

    update content

--- components/AuthorIntro.jsx
+++ components/AuthorIntro.jsx
@@ -1,25 +1,27 @@
-import { Row, Col, Card, Image, CardGroup } from "react-bootstrap";
+import { Row, Col, Media, Image } from "react-bootstrap";
 
 const AuthorIntro = () => (
   <Row>
     <Col md="8">
-      <CardGroup className="mb-4  admin-intro">
+      {/* AUTHOR INTRO STARTS */}
+      <Media className="mb-4 admin-intro">
         <Image
           roundedCircle
           width={64}
           height={64}
           className="mr-3"
-          src="https://avatars.githubusercontent.com/u/79613927?v=4"
+          src="https://avatars1.githubusercontent.com/u/9482724?s=460&u=69a6acab13fd5547a4e316e496b573271077147f&v=4"
           alt="Generic placeholder"
         />
-        <Card className="p-4 ">
+        <Media.Body>
           <h5 className="font-weight-bold mb-0">Hello Friends,</h5>
           <p className="welcome-text">
-            My name is Riko airlan and I am an experienced software engineer and
+            My name is Filip Jerga and I am an experienced software engineer and
             freelance developer. and this is my blog page.
           </p>
-        </Card>
-      </CardGroup>
+        </Media.Body>
+      </Media>
+      {/* AUTHOR INTRO ENDS */}
     </Col>
   </Row>
 );
